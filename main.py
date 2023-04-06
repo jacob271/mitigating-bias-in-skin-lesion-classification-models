8b3dafcb9a929d902353bcaa8a42bff9c7f9e5ff
Jacob Schäfer <js.jacobschaefer@gmail.com>
2023-04-06 13:08:12 +0200

    try to train resnet

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import os
+from types import SimpleNamespace
 
 import torch
 import torchvision
+from torch import optim
 from torch.utils.data import Dataset, DataLoader
 import pandas as pd
 from torchvision.io import read_image
 import matplotlib.pyplot as plt
+import torch.nn as nn
 import numpy as np
 import lightning as pl
 from torchvision.transforms import transforms
 
 classes = ["MEL", "NV", "BCC", "AKIEC", "BKL", "DF", "VASC"]
+
+act_fn_by_name = {"tanh": nn.Tanh, "relu": nn.ReLU, "leakyrelu": nn.LeakyReLU, "gelu": nn.GELU}
 
 
 class SkinLesionDataset(Dataset):
@@ -26,6 +31,7 @@ class SkinLesionDataset(Dataset):
     def __getitem__(self, idx):
         img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0] + ".jpg")
         image = read_image(img_path)
+        image = image.to(torch.float32)
         label = self.img_labels.iloc[idx][1:].astype(float).argmax()
         if self.transform:
             image = self.transform(image)
@@ -34,25 +40,251 @@ class SkinLesionDataset(Dataset):
         return image, label
 
 
+model_dict = {}
+
+
+def create_model(model_name, model_hparams):
+    if model_name in model_dict:
+        return model_dict[model_name](**model_hparams)
+    else:
+        assert False, f'Unknown model name "{model_name}". Available models are: {str(model_dict.keys())}'
+
+
+class SkinLesionModule(pl.LightningModule):
+    def __init__(self, model_name, model_hparams, optimizer_name, optimizer_hparams):
+        """
+        Inputs:
+            model_name - Name of the model/CNN to run. Used for creating the model (see function below)
+            model_hparams - Hyperparameters for the model, as dictionary.
+            optimizer_name - Name of the optimizer to use. Currently supported: Adam, SGD
+            optimizer_hparams - Hyperparameters for the optimizer, as dictionary. This includes learning rate, weight decay, etc.
+        """
+        super().__init__()
+        # Exports the hyperparameters to a YAML file, and create "self.hparams" namespace
+        self.save_hyperparameters()
+        # Create model
+        self.model = create_model(model_name, model_hparams)
+        # Create loss module
+        self.loss_module = nn.CrossEntropyLoss()
+        # Example input for visualizing the graph in Tensorboard
+        self.example_input_array = torch.zeros((1, 3, 450, 600), dtype=torch.float32)
+
+    def forward(self, imgs):
+        # Forward function that is run when visualizing the graph
+        return self.model(imgs)
+
+    def configure_optimizers(self):
+        # We will support Adam or SGD as optimizers.
+        if self.hparams.optimizer_name == "Adam":
+            # AdamW is Adam with a correct implementation of weight decay (see here
+            # for details: https://arxiv.org/pdf/1711.05101.pdf)
+            optimizer = optim.AdamW(self.parameters(), **self.hparams.optimizer_hparams)
+        elif self.hparams.optimizer_name == "SGD":
+            optimizer = optim.SGD(self.parameters(), **self.hparams.optimizer_hparams)
+        else:
+            assert False, f'Unknown optimizer: "{self.hparams.optimizer_name}"'
+
+        # We will reduce the learning rate by 0.1 after 100 and 150 epochs
+        scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[100, 150], gamma=0.1)
+        return [optimizer], [scheduler]
+
+    def training_step(self, batch, batch_idx):
+        # "batch" is the output of the training data loader.
+        imgs, labels = batch
+        preds = self.model(imgs)
+        loss = self.loss_module(preds, labels)
+        acc = (preds.argmax(dim=-1) == labels).float().mean()
+
+        # Logs the accuracy per epoch to tensorboard (weighted average over batches)
+        self.log("train_acc", acc, on_step=False, on_epoch=True)
+        self.log("train_loss", loss)
+        return loss  # Return tensor to call ".backward" on
+
+    def validation_step(self, batch, batch_idx):
+        imgs, labels = batch
+        preds = self.model(imgs).argmax(dim=-1)
+        acc = (labels == preds).float().mean()
+        # By default logs it per epoch (weighted average over batches)
+        self.log("val_acc", acc)
+
+    def test_step(self, batch, batch_idx):
+        imgs, labels = batch
+        preds = self.model(imgs).argmax(dim=-1)
+        acc = (labels == preds).float().mean()
+        # By default logs it per epoch (weighted average over batches), and returns it afterwards
+        self.log("test_acc", acc)
+
+
+def train_model(model_name, save_name=None, **kwargs):
+    """
+    Inputs:
+        model_name - Name of the model you want to run. Is used to look up the class in "model_dict"
+        save_name (optional) - If specified, this name will be used for creating the checkpoint and logging directory.
+    """
+    if save_name is None:
+        save_name = model_name
+
+    # Create a PyTorch Lightning trainer with the generation callback
+    trainer = pl.Trainer(
+        default_root_dir=os.path.join(".", save_name),  # Where to save models
+        # We run on a single GPU (if possible)
+        accelerator="auto",
+        devices=1,
+        # How many epochs to train for if no patience is set
+        max_epochs=180,
+    )  # In case your notebook crashes due to the progress bar, consider increasing the refresh rate
+    trainer.logger._log_graph = True  # If True, we plot the computation graph in tensorboard
+    trainer.logger._default_hp_metric = None  # Optional logging argument that we don't need
+
+    # Check whether pretrained model exists. If yes, load it and skip training
+    pretrained_filename = os.path.join(".", save_name + ".ckpt")
+    if os.path.isfile(pretrained_filename):
+        print(f"Found pretrained model at {pretrained_filename}, loading...")
+        # Automatically loads the model with the saved hyperparameters
+        model = SkinLesionModule.load_from_checkpoint(pretrained_filename)
+    else:
+        # pl.seed_everything(42)  # To be reproducable
+        model = SkinLesionModule(model_name=model_name, **kwargs)
+        trainer.fit(model, train_loader, val_loader)
+        model = SkinLesionModule.load_from_checkpoint(
+            trainer.checkpoint_callback.best_model_path
+        )  # Load best checkpoint after training
+
+    # Test best model on validation and test set
+    val_result = trainer.test(model, dataloaders=val_loader, verbose=False)
+    test_result = trainer.test(model, dataloaders=test_loader, verbose=False)
+    result = {"test": test_result[0]["test_acc"], "val": val_result[0]["test_acc"]}
+
+    return model, result
+
+
+class ResNetBlock(nn.Module):
+    def __init__(self, c_in, act_fn, subsample=False, c_out=-1):
+        """
+        Inputs:
+            c_in - Number of input features
+            act_fn - Activation class constructor (e.g. nn.ReLU)
+            subsample - If True, we want to apply a stride inside the block and reduce the output shape by 2 in height and width
+            c_out - Number of output features. Note that this is only relevant if subsample is True, as otherwise, c_out = c_in
+        """
+        super().__init__()
+        if not subsample:
+            c_out = c_in
+
+        # Network representing F
+        self.net = nn.Sequential(
+            nn.Conv2d(
+                c_in, c_out, kernel_size=3, padding=1, stride=1 if not subsample else 2, bias=False
+            ),  # No bias needed as the Batch Norm handles it
+            nn.BatchNorm2d(c_out),
+            act_fn(),
+            nn.Conv2d(c_out, c_out, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(c_out),
+        )
+
+        # 1x1 convolution with stride 2 means we take the upper left value, and transform it to new output size
+        self.downsample = nn.Conv2d(c_in, c_out, kernel_size=1, stride=2) if subsample else None
+        self.act_fn = act_fn()
+
+    def forward(self, x):
+        z = self.net(x)
+        if self.downsample is not None:
+            x = self.downsample(x)
+        out = z + x
+        out = self.act_fn(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(
+            self,
+            num_classes=7,
+            num_blocks=[3, 3, 3],
+            c_hidden=[16, 32, 64],
+            act_fn_name="relu",
+            block_name="ResNetBlock",
+            **kwargs,
+    ):
+        """
+        Inputs:
+            num_classes - Number of classification outputs (10 for CIFAR10)
+            num_blocks - List with the number of ResNet blocks to use. The first block of each group uses downsampling, except the first.
+            c_hidden - List with the hidden dimensionalities in the different blocks. Usually multiplied by 2 the deeper we go.
+            act_fn_name - Name of the activation function to use, looked up in "act_fn_by_name"
+            block_name - Name of the ResNet block, looked up in "resnet_blocks_by_name"
+        """
+        super().__init__()
+        self.hparams = SimpleNamespace(
+            num_classes=num_classes,
+            c_hidden=c_hidden,
+            num_blocks=num_blocks,
+            act_fn_name=act_fn_name,
+            act_fn=act_fn_by_name[act_fn_name],
+            block_class=ResNetBlock,
+        )
+        self._create_network()
+        self._init_params()
+
+    def _create_network(self):
+        c_hidden = self.hparams.c_hidden
+
+        # A first convolution on the original image to scale up the channel size
+        self.input_net = nn.Sequential(
+            nn.Conv2d(3, c_hidden[0], kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(c_hidden[0]),
+            self.hparams.act_fn(),
+        )
+
+        # Creating the ResNet blocks
+        blocks = []
+        for block_idx, block_count in enumerate(self.hparams.num_blocks):
+            for bc in range(block_count):
+                # Subsample the first block of each group, except the very first one.
+                subsample = bc == 0 and block_idx > 0
+                blocks.append(
+                    self.hparams.block_class(
+                        c_in=c_hidden[block_idx if not subsample else (block_idx - 1)],
+                        act_fn=self.hparams.act_fn,
+                        subsample=subsample,
+                        c_out=c_hidden[block_idx],
+                    )
+                )
+        self.blocks = nn.Sequential(*blocks)
+
+        # Mapping to classification output
+        self.output_net = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1, 1)), nn.Flatten(), nn.Linear(c_hidden[-1], self.hparams.num_classes)
+        )
+
+    def _init_params(self):
+        # Based on our discussion in Tutorial 4, we should initialize the convolutions according to the activation function
+        # Fan-out focuses on the gradient distribution, and is commonly used in ResNets
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity=self.hparams.act_fn_name)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        x = self.input_net(x)
+        x = self.blocks(x)
+        x = self.output_net(x)
+        return x
+
+
+model_dict["ResNet"] = ResNet
+
+
 if __name__ == "__main__":
-    train_dataset = SkinLesionDataset("./data/ISIC2018_Task3_Training_GroundTruth/ISIC2018_Task3_Training_GroundTruth"
-                                      ".csv", img_dir="./data/ISIC2018_Task3_Training_Input/")
-    train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-    test_dataset = SkinLesionDataset("./data/ISIC2018_Task3_Test_GroundTruth/ISIC2018_Task3_Test_GroundTruth.csv",
-                                     img_dir="./data/ISIC2018_Task3_Test_Input/")
-    validation_dataset = SkinLesionDataset("./data/ISIC2018_Task3_Validation_GroundTruth"
-                                           "/ISIC2018_Task3_Validation_GroundTruth.csv",
-                                           img_dir="./data/ISIC2018_Task3_Validation_Input/")
-    # Display image and label.
-    train_features, train_labels = next(iter(train_dataloader))
-    print(f"Feature batch shape: {train_features.size()}")
-    print(f"Labels batch shape: {train_labels.size()}")
-    img = train_features[0].squeeze()
-    img = np.transpose(img, (1, 2, 0))
-    label = train_labels[0]
-    print(f"Label: {classes[label]}")
-    plt.imshow(img)
-    plt.show()
+    train_set = SkinLesionDataset("./data/ISIC2018_Task3_Training_GroundTruth/ISIC2018_Task3_Training_GroundTruth"
+                                  ".csv", img_dir="./data/ISIC2018_Task3_Training_Input/")
+    test_set = SkinLesionDataset("./data/ISIC2018_Task3_Test_GroundTruth/ISIC2018_Task3_Test_GroundTruth.csv",
+                                 img_dir="./data/ISIC2018_Task3_Test_Input/")
+    val_set = SkinLesionDataset("./data/ISIC2018_Task3_Validation_GroundTruth"
+                                "/ISIC2018_Task3_Validation_GroundTruth.csv",
+                                img_dir="./data/ISIC2018_Task3_Validation_Input/")
+
     device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
     # DATA_MEANS = (train_dataset.data / 255.0).mean(axis=(0, 1, 2))
     # DATA_STD = (train_dataset.data / 255.0).std(axis=(0, 1, 2))
@@ -68,9 +300,9 @@ if __name__ == "__main__":
     )
 
     NUM_IMAGES = 4
-    images = [train_dataset[idx][0] for idx in range(NUM_IMAGES)]
+    images = [train_set[idx][0] for idx in range(NUM_IMAGES)]
     to_pil = transforms.ToPILImage()
-    orig_images = [train_dataset[idx][0] for idx in range(NUM_IMAGES)]
+    orig_images = [train_set[idx][0] for idx in range(NUM_IMAGES)]
     orig_images = [train_transform(img) for img in orig_images]
 
     img_grid = torchvision.utils.make_grid(torch.stack(images + orig_images, dim=0), nrow=4, pad_value=0.5)
@@ -83,4 +315,15 @@ if __name__ == "__main__":
     plt.show()
     plt.close()
 
+    train_loader = DataLoader(train_set, batch_size=128, shuffle=True, drop_last=True, pin_memory=True, num_workers=4)
+    val_loader = DataLoader(val_set, batch_size=128, shuffle=False, drop_last=False, num_workers=4)
+    test_loader = DataLoader(test_set, batch_size=128, shuffle=False, drop_last=False, num_workers=4)
 
+    resnet_model, resnet_results = train_model(
+        model_name="ResNet",
+        model_hparams={"num_classes": 7, "c_hidden": [16, 32, 64], "num_blocks": [3, 3, 3], "act_fn_name": "relu"},
+        optimizer_name="SGD",
+        optimizer_hparams={"lr": 0.1, "momentum": 0.9, "weight_decay": 1e-4},
+    )
+
+    print(resnet_results)
