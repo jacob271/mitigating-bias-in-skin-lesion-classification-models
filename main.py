654395eb8ee958b2a72d956d2ff5ab6d7d90d7e3
Jacob Schäfer <js.jacobschaefer@gmail.com>
2023-04-06 10:00:25 +0200

    perform data augmentation and display some examples

--- main.py
+++ main.py
@@ -53,4 +53,34 @@ if __name__ == "__main__":
     print(f"Label: {classes[label]}")
     plt.imshow(img)
     plt.show()
-    print(f"Label: {label}")
+    device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+    # DATA_MEANS = (train_dataset.data / 255.0).mean(axis=(0, 1, 2))
+    # DATA_STD = (train_dataset.data / 255.0).std(axis=(0, 1, 2))
+    # print("Data mean", DATA_MEANS)
+    # print("Data std", DATA_STD)
+    test_transform = transforms.Compose([])
+    # For training, we add some augmentation. Networks are too powerful and would overfit.
+    train_transform = transforms.Compose(
+        [
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomResizedCrop((450, 600), scale=(0.8, 1.0), ratio=(0.75, 1.33)),
+        ]
+    )
+
+    NUM_IMAGES = 4
+    images = [train_dataset[idx][0] for idx in range(NUM_IMAGES)]
+    to_pil = transforms.ToPILImage()
+    orig_images = [train_dataset[idx][0] for idx in range(NUM_IMAGES)]
+    orig_images = [train_transform(img) for img in orig_images]
+
+    img_grid = torchvision.utils.make_grid(torch.stack(images + orig_images, dim=0), nrow=4, pad_value=0.5)
+    img_grid = img_grid.permute(1, 2, 0)
+
+    plt.figure(figsize=(8, 8))
+    plt.title("Augmentation examples on training data")
+    plt.imshow(img_grid)
+    plt.axis("off")
+    plt.show()
+    plt.close()
+
+
