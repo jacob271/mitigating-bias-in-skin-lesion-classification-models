649320ba97f1b20efa98b41006dad41fa99531e5
Jacob Schäfer <js.jacobschaefer@gmail.com>
2023-04-28 09:26:55 +0200

    normalize dataset

--- main.py
+++ main.py
@@ -146,12 +146,41 @@ def visualize_example_images():
     plt.close()
 
 
+def dataset_mean_and_std():
+    # Adapted from: https://www.binarystudy.com/2022/04/how-to-normalize-image-dataset-inpytorch.html
+    train_set = SkinLesionDataset("./data/ISIC2018_Task3_Training_GroundTruth/ISIC2018_Task3_Training_GroundTruth"
+                                  ".csv", img_dir="./data/ISIC2018_Task3_Training_Input/")
+    train_loader = DataLoader(train_set, batch_size=8, shuffle=True, drop_last=True, pin_memory=False, num_workers=1)
+    cnt = 0
+    fst_moment = torch.empty(3)
+    snd_moment = torch.empty(3)
+
+    for images, _ in train_loader:
+        b, c, h, w = images.shape
+        nb_pixels = b * h * w
+        sum_ = torch.sum(images, dim=[0, 2, 3])
+        sum_of_square = torch.sum(images ** 2,
+                                  dim=[0, 2, 3])
+        fst_moment = (cnt * fst_moment + sum_) / (
+                cnt + nb_pixels)
+        snd_moment = (cnt * snd_moment + sum_of_square) / (
+                cnt + nb_pixels)
+        cnt += nb_pixels
+
+    mean, std = fst_moment, torch.sqrt(
+        snd_moment - fst_moment ** 2)
+
+    print("mean and std: \n", mean, std)
+
+
 if __name__ == "__main__":
     device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
-    # DATA_MEANS = (train_dataset.data / 255.0).mean(axis=(0, 1, 2))
-    # DATA_STD = (train_dataset.data / 255.0).std(axis=(0, 1, 2))
-    # print("Data mean", DATA_MEANS)
-    # print("Data std", DATA_STD)
+
+    DATA_MEANS = torch.tensor([194.6954, 139.2556, 145.4731])
+    DATA_STD = torch.tensor([36.0131, 38.9913, 43.4326])
+    print("Data mean", DATA_MEANS)
+    print("Data std", DATA_STD)
+
     test_transform = transforms.Compose([transforms.Resize((32, 32), antialias=True)])
     # For training, we add some augmentation. Networks are too powerful and would overfit.
     train_transform = transforms.Compose(
@@ -159,6 +188,7 @@ if __name__ == "__main__":
             transforms.Resize((32, 32), antialias=True),
             transforms.RandomHorizontalFlip(),
             transforms.RandomResizedCrop((32, 32), scale=(0.8, 1.0), ratio=(0.75, 1.33), antialias=True),
+            transforms.Normalize(DATA_MEANS, DATA_STD),
         ]
     )
 
