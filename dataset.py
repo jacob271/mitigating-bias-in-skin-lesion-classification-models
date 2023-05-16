5141aac752fb5ee944eb6d96beb076270e625e9b
Jacob Schäfer <js.jacobschaefer@gmail.com>
2023-05-16 10:46:05 +0200

    reformat file and add val and test set with metadata

--- dataset.py
+++ dataset.py
@@ -74,16 +74,12 @@ class SkinLesionDataset(Dataset):
 DATA_MEANS = torch.tensor([194.7155, 139.2602, 145.4779])
 DATA_STD = torch.tensor([36.0167, 38.9894, 43.4381])
 
-test_transform = transforms.Compose([
-    transforms.Normalize(DATA_MEANS, DATA_STD)])
+test_transform = transforms.Compose([transforms.Normalize(DATA_MEANS, DATA_STD)])
 
-train_transform = transforms.Compose(
-    [
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomResizedCrop((650, 400), scale=(0.8, 1.0), ratio=(0.75, 1.33), antialias=True),
-        transforms.Normalize(DATA_MEANS, DATA_STD),
-    ]
-)
+train_transform = transforms.Compose([transforms.RandomHorizontalFlip(),
+                                      transforms.RandomResizedCrop((650, 400), scale=(0.8, 1.0), ratio=(0.75, 1.33),
+                                                                   antialias=True),
+                                      transforms.Normalize(DATA_MEANS, DATA_STD), ])
 
 train_set = SkinLesionDataset("./data/ISIC2018_Task3_Training_GroundTruth/ISIC2018_Task3_Training_GroundTruth.csv",
                               img_dir="./data/ISIC2018_Task3_Training_Input/",
@@ -91,25 +87,31 @@ train_set = SkinLesionDataset("./data/ISIC2018_Task3_Training_GroundTruth/ISIC20
                               transform=train_transform)
 train_set_with_metadata = SkinLesionDataset(
     "./data/ISIC2018_Task3_Test_GroundTruth/ISIC2018_Task3_Test_GroundTruth.csv",
-    img_dir="./data/ISIC2018_Task3_Test_Input/",
-    metadata_file="./data/ISIC2018_Task3_Test_GroundTruth/metadata.csv",
-    include_metadata=True,
-    transform=test_transform)
+    img_dir="./data/ISIC2018_Task3_Test_Input/", metadata_file="./data/ISIC2018_Task3_Test_GroundTruth/metadata.csv",
+    include_metadata=True, transform=test_transform)
 test_set = SkinLesionDataset("./data/ISIC2018_Task3_Test_GroundTruth/ISIC2018_Task3_Test_GroundTruth.csv",
                              img_dir="./data/ISIC2018_Task3_Test_Input/",
                              metadata_file="./data/ISIC2018_Task3_Test_GroundTruth/metadata.csv",
                              transform=test_transform, under_sampling=False)
+test_set_with_metadata = SkinLesionDataset("./data/ISIC2018_Task3_Test_GroundTruth/ISIC2018_Task3_Test_GroundTruth.csv",
+                                           img_dir="./data/ISIC2018_Task3_Test_Input/",
+                                           metadata_file="./data/ISIC2018_Task3_Test_GroundTruth/metadata.csv",
+                                           transform=test_transform, under_sampling=False, include_metadata=True)
 val_set = SkinLesionDataset("./data/ISIC2018_Task3_Validation_GroundTruth/ISIC2018_Task3_Validation_GroundTruth.csv",
                             img_dir="./data/ISIC2018_Task3_Validation_Input/",
                             metadata_file="./data/ISIC2018_Task3_Validation_GroundTruth/metadata.csv",
                             transform=test_transform, under_sampling=False)
+val_set_with_metadata = SkinLesionDataset(
+    "./data/ISIC2018_Task3_Validation_GroundTruth/ISIC2018_Task3_Validation_GroundTruth.csv",
+    img_dir="./data/ISIC2018_Task3_Validation_Input/",
+    metadata_file="./data/ISIC2018_Task3_Validation_GroundTruth/metadata.csv", transform=test_transform,
+    under_sampling=False, include_metadata=True)
 
 
 def dataset_mean_and_std():
     # Adapted from: https://www.binarystudy.com/2022/04/how-to-normalize-image-dataset-inpytorch.html
     data_set = SkinLesionDataset("./data/ISIC2018_Task3_Training_GroundTruth/ISIC2018_Task3_Training_GroundTruth"
-                                 ".csv",
-                                 metadata_file="./data/ISIC2018_Task3_Training_GroundTruth/metadata.csv",
+                                 ".csv", metadata_file="./data/ISIC2018_Task3_Training_GroundTruth/metadata.csv",
                                  img_dir="./data/ISIC2018_Task3_Training_Input/")
     # transform=transforms.Compose([transforms.Resize((32, 32), antialias=True)]))
     data_loader = DataLoader(data_set, batch_size=64, shuffle=True, drop_last=True, pin_memory=False, num_workers=1)
@@ -121,24 +123,19 @@ def dataset_mean_and_std():
         b, c, h, w = images.shape
         nb_pixels = b * h * w
         sum_ = torch.sum(images, dim=[0, 2, 3])
-        sum_of_square = torch.sum(images ** 2,
-                                  dim=[0, 2, 3])
-        fst_moment = (cnt * fst_moment + sum_) / (
-                cnt + nb_pixels)
-        snd_moment = (cnt * snd_moment + sum_of_square) / (
-                cnt + nb_pixels)
+        sum_of_square = torch.sum(images ** 2, dim=[0, 2, 3])
+        fst_moment = (cnt * fst_moment + sum_) / (cnt + nb_pixels)
+        snd_moment = (cnt * snd_moment + sum_of_square) / (cnt + nb_pixels)
         cnt += nb_pixels
 
-    mean, std = fst_moment, torch.sqrt(
-        snd_moment - fst_moment ** 2)
+    mean, std = fst_moment, torch.sqrt(snd_moment - fst_moment ** 2)
 
     print("mean and std: \n", mean, std)
 
 
 def visualize_example_images():
     data_set = SkinLesionDataset("./data/ISIC2018_Task3_Training_GroundTruth/ISIC2018_Task3_Training_GroundTruth"
-                                 ".csv",
-                                 metadata_file="./data/ISIC2018_Task3_Training_GroundTruth/metadata.csv",
+                                 ".csv", metadata_file="./data/ISIC2018_Task3_Training_GroundTruth/metadata.csv",
                                  img_dir="./data/ISIC2018_Task3_Training_Input/")
     num_images = 4
     images = [data_set[idx][0] / 255.0 for idx in range(num_images)]
