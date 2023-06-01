7d67c6c4107d18f30457d3cb4ee94a6e12b36491
Jacob Schäfer <js.jacobschaefer@gmail.com>
2023-06-01 19:02:00 +0200

    clean up resnet_training.py

--- resnet_training.py
+++ resnet_training.py
@@ -17,26 +17,23 @@ CHECKPOINT_PATH = os.environ.get("PATH_CHECKPOINT", "./saved_models")
 os.makedirs(CHECKPOINT_PATH, exist_ok=True)
 
 
-def train_model():
+def train_resnet(debiasing=False):
     save_name = "ResNet"
     print("saving to ", os.path.join(CHECKPOINT_PATH, save_name))
 
-    # Create a PyTorch Lightning trainer with the generation callback
     trainer = pl.Trainer(
-        default_root_dir=os.path.join(CHECKPOINT_PATH, save_name),  # Where to save models
-        # We run on a single GPU (if possible)
+        default_root_dir=os.path.join(CHECKPOINT_PATH, save_name),
         accelerator="auto",
         devices=1,
-        # How many epochs to train for if no patience is set
         max_epochs=180,
         callbacks=[
             ModelCheckpoint(
                 save_weights_only=True, mode="max", monitor="val_acc"
-            ),  # Save the best checkpoint based on the maximum val_acc recorded. Saves only weights and not optimizer]
+            ),  # Save the best checkpoint based on the maximum val_acc recorded
         ],
         logger=wandb_logger
-    )  # In case your notebook crashes due to the progress bar, consider increasing the refresh rate
-    train_set = get_dataset("train", under_sampling=True, use_sample_probabilities=True)
+    )
+    train_set = get_dataset("train", under_sampling=True, use_sample_probabilities=debiasing)
     val_set = get_dataset("validation")
     test_set = get_dataset("test")
     train_loader = DataLoader(train_set, batch_size=32, shuffle=False, drop_last=False, pin_memory=False, num_workers=1)
@@ -52,11 +49,11 @@ def train_model():
     # Test best model on validation and test set
     val_result = trainer.test(model, dataloaders=val_loader, verbose=False)
     test_result = trainer.test(model, dataloaders=test_loader, verbose=False)
-    result = {"test": test_result[0]["test_acc"], "val": val_result[0]["test_acc"]}
+    result = {"test": test_result, "val": val_result}
 
     return model, result
 
 
 if __name__ == "__main__":
-    resnet_model, resnet_results = train_model()
+    resnet_model, resnet_results = train_resnet(debiasing=True)
     print(resnet_results)
