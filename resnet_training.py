c3949f8fd2541eb23282a849ea96035eed6aa935
Jacob Schäfer <js.jacobschaefer@gmail.com>
2023-06-04 15:45:12 +0200

    perform bias evaluation immediately after training for resnets

--- resnet_training.py
+++ resnet_training.py
@@ -2,9 +2,17 @@ import os
 
 from pytorch_lightning.callbacks import ModelCheckpoint
 import pytorch_lightning as pl
-
+import torchmetrics.classification
+from torch.utils.data import DataLoader
+from torchmetrics import ConfusionMatrix
 from pytorch_lightning.loggers import WandbLogger
 from torch.utils.data import DataLoader
+import wandb
+import torch
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from statistics import variance
 
 from dataset import get_dataset
 from resnet_module import ResNetModel
@@ -54,6 +62,129 @@ def train_resnet(debiasing=False):
     return model, result
 
 
+def get_predictions(model, data_set_name="test"):
+    device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+    model.to(device)
+    
+    trainer = pl.Trainer(
+        accelerator="auto",
+        devices=1,
+        logger=False,
+    )
+
+    data_set_with_metadata = get_dataset(data_set_name, include_metadata=True)
+    data_loader = DataLoader(data_set_with_metadata, batch_size=1, shuffle=False, drop_last=False, num_workers=1)
+
+    all_labels = []
+    for batch in data_loader:
+        imgs, labels = batch
+        all_labels.append(labels)
+
+    #print(all_labels)
+    predictions = trainer.predict(model, data_loader)
+    predictions = torch.cat(predictions)
+    #print(predictions)
+    return predictions, all_labels
+
+
+def plot_confusion_matrix(predictions, all_labels, file_path="conf_ma.png"):
+    class_labels = []
+    for label in all_labels:
+        class_labels.append(label[0])
+    class_labels = torch.cat(class_labels)
+    
+    confm = ConfusionMatrix(task="multiclass", num_classes=4)
+    result = confm(predictions, class_labels)
+
+    labels = ["MEL", "NV", "BCC", "BKL"]
+    sns.heatmap(result, annot=True, cmap="Blues", xticklabels=labels, yticklabels=labels)
+    plt.xlabel("Predicted")
+    plt.ylabel("True")
+    plt.title("Confusion Matrix")
+    plt.savefig(file_path)
+
+    
+def calculate_gender_bias(predictions, all_labels):
+    male_predictions = []
+    male_labels = []
+    female_predictions = []
+    female_labels = []
+    unknown_count = 0
+    
+    for i in range(len(predictions)):
+        if all_labels[i][2][0] == 'male':
+            male_predictions.append(torch.unsqueeze(predictions[i], dim=0))
+            male_labels.append(all_labels[i][0])
+        elif all_labels[i][2][0] == 'female':
+            female_predictions.append(torch.unsqueeze(predictions[i], dim=0))
+            female_labels.append(all_labels[i][0])
+        else:
+            unknown_count += 1
+    print(f"Observed {unknown_count} labels out of {len(predictions)} to be unknown")
+
+    metric = torchmetrics.classification.MulticlassAccuracy(num_classes=4, average='weighted')
+    male_accuracy = metric(torch.cat(male_predictions), torch.cat(male_labels)).item()
+    female_accuracy = metric(torch.cat(female_predictions), torch.cat(female_labels)).item()
+    bias = variance([male_accuracy, female_accuracy])
+    results = {"male_acc": male_accuracy, "female_acc": female_accuracy, "bias": bias}
+    print(f"male_acc: {male_accuracy}")
+    print(f"female_acc: {female_accuracy}")
+    print(f"bias: {bias}")
+    return results
+
+
+def calculate_age_bias(predictions, all_labels):
+    age_groups = ["upto30", "35to55", "60up", "unknown"]
+    age_based_predictions = {"upto30": [], "35to55": [], "60up": [], "unknown": []}
+    age_labels = {"upto30": [], "35to55": [], "60up": [], "unknown": []}
+    
+    unknown_counter = 0
+    for i in range(len(predictions)):
+        age = all_labels[i][1][0].item()
+        if age <= 0.0:
+            age_group = "unknown"
+            unknown_counter += 1
+        if age <= 30.0:
+            age_group = "upto30"
+        elif age <= 55.0:
+            age_group = "35to55"
+        else:
+            age_group = "60up"
+
+        age_based_predictions[age_group].append(torch.unsqueeze(predictions[i], dim=0))
+        age_labels[age_group].append(all_labels[i][0])
+        
+    print(f"Observed {unknown_counter} ages out of {len(predictions)} to be unknown")
+    
+    metric = torchmetrics.classification.MulticlassAccuracy(num_classes=4, average='weighted')
+
+    accuracies = {}
+    acc_list = []
+    for age_group in age_groups:
+        print(f"{age_group} has {len(age_based_predictions[age_group])} samples")
+        if age_group == "unknown":
+            continue
+        if len(age_based_predictions[age_group]) > 0:
+            accuracies[age_group] = metric(torch.cat(age_based_predictions[age_group]), torch.cat(age_labels[age_group])).item()
+            acc_list.append(accuracies[age_group])
+        else:
+            print("WARNING: No samples for this age group")
+
+    results = {"accuracies": accuracies, "bias": variance(acc_list)}
+    print(f"accuracies: {accuracies}")
+    print(f"bias: {variance(acc_list)}")
+    return results
+
+
 if __name__ == "__main__":
     resnet_model, resnet_results = train_resnet(debiasing=False)
+    predictions, all_labels = get_predictions(resnet_model)
+    confm_path = "conf_matrix.png"
+    plot_confusion_matrix(predictions, all_labels, confm_path)
+    wandb.log({"confusion matrix": wandb.Image(conf_path)})
+    gender_bias = calculate_gender_bias(predictions, all_labels)
+    wandb.log(gender_bias)
+    age_bias = calculate_age_bias(predictions, all_labels)
+    wandb.log(age_bias)    
+    
     print(resnet_results)
